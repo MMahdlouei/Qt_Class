31551865d4bd73a8cf804f1b333fea9842b41211
Majid <mahdlooyi@gmail.com>
2023-10-13 21:19:26 +0330

    Gui updated

--- MRI/mainwindow.py
+++ MRI/mainwindow.py
@@ -1,11 +1,10 @@
 # This Python file uses the following encoding: utf-8
-from pickle import TRUE
 import sys
 import os
 
 from PySide2.QtCore import QTimer
 from random import randrange
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog
 import pyqtgraph as pg
 import threading
 
@@ -32,7 +31,8 @@ class MainWindow(QMainWindow):
         #self.ui.pushButton_2.clicked.connect(self.timer.stop)
         self.ui.tabWidget.currentChanged.connect(self.TabChanged)
    
-        
+        self.ui.btnbrowse.clicked.connect(self.BrowseFile)
+        self.ui.btnavarage.clicked.connect(self.Avar)
 
         # circular buffer for storing serial data until it is
         # fetched by the GUI
@@ -41,6 +41,13 @@ class MainWindow(QMainWindow):
 
         # self.plot([1,2,3,4,5,6,7,8,9,10], [30,32,34,32,33,31,29,32,35,45])
         
+    def Avar(self):
+        if(self.ui.btnavarage.isChecked()):
+            print("dasjagh")
+            self.j = 10
+        else:
+            print("sih")
+    
     def TabChanged(self, index):
         if(index == self.ui.tabWidget.indexOf(self.ui.tab)):
             self.timer.stop()
@@ -53,7 +60,7 @@ class MainWindow(QMainWindow):
         
         if True:
             # see whether an exit was requested
-            self.j=self.j+1
+            
             hour=[1, 2, 3, 4]      
             temperature=[1, 2, 3, randrange(10)] 
 
@@ -65,19 +72,34 @@ class MainWindow(QMainWindow):
 
             else:
                 self.plotItem.setData(hour, temperature)
-                print("amjikh")
-
+              
             # self.ui.graphWidget_2.plot(hour, temperature)
             # self.ui.graphWidget_3.plot(hour, temperature)
 
 
+    def BrowseFile(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly  # Allow read-only access
+        file_dialog = QFileDialog(self)
+        file_dialog.setOptions(options)
 
+        # Use getOpenFileName to allow selecting a single file
+        # Use getOpenFileNames to allow selecting multiple files
+        file_dialog.setFileMode(QFileDialog.ExistingFiles)
+        filepath = file_dialog.getOpenFileName(self, "Open File", "", "All Files (*);;Text Files (*.txt)")
+
+        # Print the selected files
+
+        
+        self.file = open(filepath[0], 'r')
+        self.ui.WorkingDirectory.setText(filepath[0])
+        content = self.file.read()
+        print(content)
 
     def Run(self):
         self.ui.tabWidget.setCurrentIndex(self.ui.tabWidget.indexOf(self.ui.tab_2)) 
         hour=[1,2,3,4,5,6,7,8,9,10]     
         temperature=[30,32,34,32,33,31,29,32,35,45]
-
   #      thread = threading.Thread(target=self.plott, args=(hour,temperature,))
  #       thread.start()
         #self.timer.start(20)
